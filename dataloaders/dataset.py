fa401e91b4e0d2b5027d439244983b726551d07d
wojciechbatruszewicz <wojtek.baltruszewicz@gmail.com>
2023-12-13 22:30:00 +0100

    Add Loading filenames with labels to main.py and trainer.py, model.py empty files.

---
+++ dataloaders/dataset.py
@@ -0,0 +1,6 @@
+import torch
+
+class DatasetGTZAN():
+    def __init__(self, path_to_images: str) -> None:
+        self.path_to_images = path_to_images
+    
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 """ Main module for managing program flow."""
 import logging
+import argparse
+import os
+import random
 
 import torch
+from sklearn.model_selection import train_test_split
 
 from model.model import AMGCModel
 
@@ -16,13 +20,35 @@ def set_device():
     logging.info(" Device: %s", device)
     return device
 
-def main():
+def load_filenames_and_labels_gtzan(images_dir: str):
+    genres = os.listdir(images_dir)
+    data = []
+    for genre in genres:
+        genre_dir = os.path.join(images_dir, genre)
+        filenames = os.listdir(genre_dir)
+        for filename in filenames:
+            path_to_filename = os.path.join(genre_dir, filename)
+            data.append((path_to_filename, genre))
+    return data
+
+
+def main(images_dir: str) -> None:
     """Main function."""
     device = set_device()
     model = AMGCModel(num_classes=10)
     logging.info(" Model architecture: %s", model)
     model.to(device)
+    data = load_filenames_and_labels_gtzan(images_dir)
+    random.shuffle(data)
+
+    train, temp = train_test_split(data, test_size=0.3)
+    validation, test = train_test_split(temp, test_size=0.5)
+
+    
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
-    main()
+    argument_parser = argparse.ArgumentParser()
+    argument_parser.add_argument("-id", "--images-dir", help="Path to root_dir of images.")
+    args = argument_parser.parse_args()
+    main(args.images_dir)
